49a54f94e9498df78e21bef94f65483ad57e9187
Swati <115008404+swatimry@users.noreply.github.com>
2025-01-03 17:23:07 +0530

    Update LoginPage.java

--- src/main/java/com/example/automation/LoginPage.java
+++ src/main/java/com/example/automation/LoginPage.java
@@ -7,35 +7,35 @@ import org.openqa.selenium.WebElement;
 public class LoginPage {
     private final WebDriver driver;
 
-    // Constructor to initialize WebDriver
+   
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    // Navigate to the login page
+   
     public void openLoginPage(String url) {
         driver.get(url);
     }
 
-    // Enter username
+   
     public void setUsername(String username) {
         WebElement usernameField = driver.findElement(By.id("username"));
         usernameField.sendKeys(username);
     }
 
-    // Enter password
+    
     public void setPassword(String password) {
         WebElement passwordField = driver.findElement(By.id("password"));
         passwordField.sendKeys(password);
     }
 
-    // Click the login button
+    
     public void clickLoginButton() {
         WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
         loginButton.click();
     }
 
-    // Get the success message for validation
+    
     public String getSuccessMessage() {
         WebElement messageElement = driver.findElement(By.id("flash"));
         return messageElement.getText();
@@ -43,7 +43,7 @@ public class LoginPage {
 
     
 
-    // Get the error message for validation
+    
     public String getErrorMessage() {
         WebElement errorElement = driver.findElement(By.id("error"));
         return errorElement.getText();
